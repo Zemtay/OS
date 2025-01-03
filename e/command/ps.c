8445aa938ce00dd6e5582574ba7cc694d85033df
Zemtay <112751326+Zemtay@users.noreply.github.com>
2025-01-03 22:43:40 +0800

    Delete e directory

--- e/command/ps.c
+++
@@ -1,6 +0,0 @@
-#include "type.h"
-#include "stdio.h"
-
-int main(int argc, char* argv[]) {
-    getpinfo();
-}
--- e/command/test.c
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-
-int main(int argc, char * argv[])
-{
-	printf("hello, our team members are:\n");
-	printf("ZhangTianyi, LuoYanyi, Jiasongru, ZhaoYilin. \n");
-	return 0;
-}
